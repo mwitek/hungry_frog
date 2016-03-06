cef98023bf99fdd248be3382924f477bc69ad44b
Matthew Witek <matt@mwitekdesign.com>
2016-03-06 02:13:47 -0500

    add collision and particle effects to fly

---
+++ project/Assets/Scripts/FlyPickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyPickup : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject pickupPrefab;
+
+	void OnTriggerEnter(Collider other) {
+		//if the other object intering the fly is Player, do stuff
+		if (other.CompareTag ("Player")) {
+			// add particle effect
+			Instantiate(pickupPrefab, transform.position, Quaternion.identity);
+
+			Destroy (gameObject);
+			FlySpawner.totalFlies--;
+		}
+	}
+}
---
+++ project/Assets/Scripts/PickupParticlesDestruction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupParticlesDestruction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Destroy particle effect after it plays
+		Destroy(gameObject, 2f);
+	}
+}
