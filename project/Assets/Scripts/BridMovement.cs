87268d0ff6fc5ab2482b6e33d518bcced30f32f2
Matthew Witek <matt@mwitekdesign.com>
2016-03-06 02:45:01 -0500

    Add bird to chase down frog

---
+++ project/Assets/Scripts/BridMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BridMovement : MonoBehaviour {
+
+	[SerializeField]
+	private Transform target;
+	private NavMeshAgent birdAgent;
+	private Animator birdAnimator;
+
+	// Use this for initialization
+	void Start () {
+		birdAgent = GetComponent<NavMeshAgent> ();
+		birdAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// set bird desitation
+		birdAgent.SetDestination (target.position);
+
+		//measure magnitude of the NavMeshAgent velocity
+		float speed = birdAgent.velocity.magnitude;
+
+		// Pass vilocity to animator
+
+		birdAnimator.SetFloat ("Speed", speed);
+	}
+}
